04444ea97a4c3f15c2603472d75a9bd6dbda77cc
Aki Hayakawa <71726587+GunDevi1@users.noreply.github.com>
2024-03-15 19:17:43 +0200

    add scroll reveal, finish footer and other edits

---
+++ Web-Portfolio/assets/vendor/scrollreveal.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.home__greeting')
+ScrollReveal().reveal('.home__img-block')
+ScrollReveal().reveal('.projects__item')
+ScrollReveal().reveal('.footer__info')
+ScrollReveal().reveal('.footer__img')
